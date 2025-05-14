238664b316a20d1356a8764918086e752dbb6aa6
mahmoud9877 <mohmoudali9877@gmail.com>
2025-05-14 05:05:46 +0300

    start

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "./config/.env") });
 const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
